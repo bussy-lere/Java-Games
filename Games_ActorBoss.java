9587d15cdfea46367778a6bc73e5a9d0ac09f4b7
bussy-lere <oluwabusayomi123@gmail.com>
2020-06-12 23:50:43 +0100

    Add files via upload

---
+++ Games_ActorBoss.java
@@ -0,0 +1,30 @@
+public class Games_ActorBoss {
+
+	
+	public static void main(String[] args) {
+		int A_life=4;
+		int B1_life=2;
+		int B2_life=2;
+		Boolean A_dead = true;
+		Boolean B1_dead = true;
+		Boolean B2_dead = true;
+		 
+		
+		if(A_dead= true){
+			A_life= A_life-1;
+			System.out.printf("You have %d lives remaining", A_life);
+			if(A_life == 0){
+				System.out.println("You have no life, you are dead");
+				}
+			}
+		
+		if(B1_dead= true){
+			A_life= A_life-1;
+			System.out.printf("You have %d livesremaining", A_life);
+			if(A_life == 0){
+				System.out.println("You have no life, you are dead");
+				}
+			}
+	}
+
+}
